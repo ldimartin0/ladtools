ddb883a0dae4a416e5350cf46c6019d0f40b7e60
ldimartin0 <luke.dnu@gmail.com>
2022-04-06 16:16:00 -0400

    Initial commit

---
+++ geom_lm.R
@@ -0,0 +1,10 @@
+geom_lm <- function (mapping = NULL, data = NULL, stat = "smooth", position = "identity", ..., method = "lm", formula = y ~ x, se = FALSE, na.rm = FALSE, orientation = NA, show.legend = NA, inherit.aes = TRUE) 
+{
+	params <- list(na.rm = na.rm, orientation = orientation, 
+								 se = se, ...)
+	if (identical(stat, "smooth")) {
+		params$method <- method
+		params$formula <- formula
+	}
+	layer(data = data, mapping = mapping, stat = stat, geom = GeomSmooth, position = position, show.legend = show.legend, inherit.aes = inherit.aes, params = params)
+}
