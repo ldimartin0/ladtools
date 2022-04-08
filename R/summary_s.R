9238f29ba651ce33172a92e25b6870a9a781d3cc
ldimartin0 <luke.dnu@gmail.com>
2022-04-08 14:05:28 -0400

    More new functions, edited description

--- R/summary_s.R
+++ R/summary_s.R
@@ -1,22 +1,23 @@
 summary_s <- function(lm) {
+	cat(paste("Dependent Var:", lm$terms[[2]]), "\n")
+
 	df_p <- parameters::parameters(lm)
 	names(df_p)[1:2] <- c("Term", "Coef")
 	df_p <- dplyr::select(df_p, -c(CI, df_error))
-	df_p <- dplyr::mutate(df_p, dplyr::across(where(is.numeric), ~round(., digits = 3)))
+	df_p <- dplyr::mutate(df_p, dplyr::across(where(is.numeric), ~format(round(., digits = 2))))
 	df_p$Term[df_p$Term == "(Intercept)"] <- "Intcpt"
+
+
 	paint::paint(df_p, paint_align_row_head = "center")
 
 	df_g <- glance(lm)
 
-	df_g_cols <- tibble(
-		`r.sq + adj.r.sq` = c(df_g$r.squared, df_g$adj.r.squared),
-		`stat + p-val` = c(df_g$statistic, df_g$p.value),
-		`AIC + BIC`= c(df_g$AIC, df_g$BIC),
-		`df + n` = c(df_g$df, df_g$nobs))
+	df_g <- dplyr::mutate(df_g, dplyr::across(where(is.numeric), ~format(round(., digits = 2))))
 
-	paint::paint(df_g_cols)
-
-	df_g_single <- dplyr::select(df_g, c(sigma, deviance))
-	paint::paint(df_g_single)
-
+	cat(crayon::italic("r.sq + adj.r.sq: ") %+% " " %+% crayon::silver(df_g$r.squared) %+% " " %+% crayon::blue(df_g$adj.r.squared) %+% "\n")
+	cat(crayon::italic("stat + p-val: ") %+% " " %+% crayon::silver(df_g$statistic) %+% " " %+% crayon::blue(df_g$p.value) %+% "\n")
+	cat(crayon::italic("AIC + BIC: ") %+% " " %+% crayon::silver(df_g$AIC) %+% " " %+% crayon::blue(df_g$BIC) %+% "\n")
+	cat(crayon::italic("df + n: ") %+% " " %+% crayon::silver(df_g$df) %+% " " %+%  crayon::blue(df_g$nobs) %+% "\n")
+	cat(crayon::italic("sigma: ") %+% " " %+% crayon::silver(df_g$sigma) %+% "\n")
+	cat(crayon::italic("deviance: ") %+% " " %+% crayon::silver(df_g$deviance) %+% "\n")
 }
