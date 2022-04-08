caf69557d091ccbc482489f1f6e1cb67f9ad20db
ldimartin0 <luke.dnu@gmail.com>
2022-04-08 14:41:36 -0400

    Added documentation

--- R/is_increasing.R
+++ R/is_increasing.R
@@ -1,12 +1,21 @@
+#' Determine Whether a Vector is Monotonically Increasing or Decreasing
+#'
+#' `is_increasing()` and `is_decreasing()` are trivial wrapper functions that determine whether a numerical vector is increasing or decreasing
+#' @param vec
+#'
+#' @return
+#' @export
+#'
+#' @examples
 is_increasing <- function(vec) {
-	if (!is.numeric(vec)) {stop("Vector passed to is.increasing() must be numeric", .call = TRUE)}
+	if (!is.numeric(vec)) {stop("Vector must be numeric", .call = TRUE)}
 
 	out <- all(vec == cummax(vec))
 	return(out)
 }
 
 is_decreasing <- function(vec) {
-	if (!is.numeric(vec)) {stop("Vector passed to is.increasing() must be numeric", .call = TRUE)}
+	if (!is.numeric(vec)) {stop("Vector must be numeric", .call = TRUE)}
 
 	out <- all(vec == cummin(vec))
 	return(out)
